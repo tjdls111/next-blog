0df2e52489aa1869f5f735124a4b6438eefa3cb3
tjdls111 <tjdls111@naver.com>
2023-06-10 21:22:57 +0900

    style:header

--- blog/src/components/Header.tsx
+++ blog/src/components/Header.tsx
@@ -1,15 +1,18 @@
+'use client'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Header = () => {
+const pathname=usePathname();
 return (
     <header className="flex justify-between mx-3 py-3 border-b-2 border-b-gray-500">
       <h1>
         <Link href="/">Snrnsidy</Link>
       </h1>
       <nav className="flex gap-3">
-        <Link href="/about">About</Link>
-        <Link href="/post">Post</Link>
+        <Link href="/about" className={pathname.includes('about')?'bg-gray-400':''}>About</Link>
+        <Link href="/post" className={pathname.includes('post')?'bg-gray-400':''}>Post</Link>
       </nav>
     </header>
 );
