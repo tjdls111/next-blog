f2cd0ad2a898f685d3b853b53481e0a1ba0ccabf
tjdls111 <tjdls111@naver.com>
2023-07-15 14:49:26 +0900

    style: post list

--- blog/src/app/post/page.tsx
+++ blog/src/app/post/page.tsx
@@ -1,28 +1,7 @@
-'use client'
 import { useNewList } from '@/adapters/useCase/useNewList'
-import Link from 'next/link'
-import React from 'react'
+import { List } from '@/components/post/PostListFetcher'
 
 const PostList = () => {
-  const { data, error, isLoading, size, setSize } = useNewList()
-
-  if (isLoading) return <div>loading...</div>
-  if (error || !data) return <div>failed to load</div>
-  return (
-    <>
-      <ul>
-        {data.map((items, index) => {
-          return items.map((item) => (
-            <li key={item.id}>
-              <Link href={item.url} target="_blank">
-                {item.title} ({item.time_ago})
-              </Link>
-            </li>
-          ))
-        })}
-      </ul>
-      <button onClick={() => setSize(size + 1)}>Load More</button>
-    </>
-  )
+  return <List />
 }
 export default PostList
