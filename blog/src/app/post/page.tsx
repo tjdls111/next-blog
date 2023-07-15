14182d3cacd2f552dcea05278b29ab730c0cb9d0
tjdls111 <tjdls111@naver.com>
2023-07-15 13:09:43 +0900

    feat: eslint, prettier, swr

--- blog/src/app/post/page.tsx
+++ blog/src/app/post/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import useSWR from 'swr';
 
 const PostList = () => {
-return (
-    <div>PostList</div>
-);
-}
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR('/api/user/123', fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return <div>PostList</div>;
+};
 export default PostList;
