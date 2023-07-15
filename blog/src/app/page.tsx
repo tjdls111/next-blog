6f84fdc8824fd1b665394f28d18b49fe4be2e133
tjdls111 <tjdls111@naver.com>
2023-07-15 19:02:32 +0900

    feat: main page

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  
-    </main>
+    <section className="flex flex-col items-center my-5">
+      <Image src="/images/avatar.png" alt="avatar" width="100" height="100" />
+      <p className="mt-2 text-blog text-lg">Hello, I am Snrnsidy.</p>
+      <p className="mt-2 text-sm">Algorithm, PS 💖</p>
+      <button className="px-3 py-2 rounded mt-2 bg-pink">
+        <Link href="/contact">Contact Me</Link>
+      </button>
+    </section>
   )
 }
