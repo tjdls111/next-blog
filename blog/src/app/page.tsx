4bb0a0fe05f8133585de669fcd26e3547ac8f92c
tjdls111 <tjdls111@naver.com>
2023-07-15 20:18:18 +0900

    refactor: profile 컴포넌트 분리

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,15 +1,7 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Profile from '@/components/home/Profile'
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center my-5">
-      <Image src="/images/avatar.png" alt="avatar" width="100" height="100" />
-      <p className="mt-2 text-blog text-lg">Hello, I am Snrnsidy.</p>
-      <p className="mt-2 text-sm">Algorithm, PS 💖</p>
-      <button className="px-3 py-2 rounded mt-2 bg-pink">
-        <Link href="/contact">Contact Me</Link>
-      </button>
-    </section>
+      <Profile />
   )
 }
