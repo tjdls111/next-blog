ceaf7697e7586967fa2231b95f104395a6e11483
tjdls111 <tjdls111@naver.com>
2023-07-15 14:12:44 +0900

    feat:pagination

--- blog/src/adapters/useCase/useNewList.ts
+++ blog/src/adapters/useCase/useNewList.ts
@@ -1,16 +1,24 @@
-import { NewsList } from '@/type/newsList';
-import useSWR from 'swr';
+import { NewsList } from '@/type/newsList'
+import useSWRInfinite from 'swr/infinite'
+
+const getKey = (pageIndex: number, previousPageData: any) => {
+  if (previousPageData && !previousPageData.length) return null // 끝에 도달
+  return `https://api.hnpwa.com/v0/news/${pageIndex + 1}.json` // SWR 키
+}
+
 export const useNewList = () => {
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const fetcher = (url: string) => fetch(url).then((res) => res.json())
   //TODO: api 경로 변경(지금은 hacker news)
-  const { data, error, isLoading } = useSWR<NewsList[]>(
-    'https://api.hnpwa.com/v0/news/1.json',
+  const { data, error, isLoading, size, setSize } = useSWRInfinite<NewsList[]>(
+    getKey,
     fetcher
-  );
+  )
 
   return {
     data,
     isLoading,
     error,
-  };
-};
+    size,
+    setSize,
+  }
+}
--- blog/src/app/post/page.tsx
+++ blog/src/app/post/page.tsx
@@ -1,23 +1,28 @@
-'use client';
-import { useNewList } from '@/adapters/useCase/useNewList';
-import Link from 'next/link';
-import React from 'react';
+'use client'
+import { useNewList } from '@/adapters/useCase/useNewList'
+import Link from 'next/link'
+import React from 'react'
 
 const PostList = () => {
-  const { data, error, isLoading } = useNewList();
+  const { data, error, isLoading, size, setSize } = useNewList()
 
-  if (isLoading) return <div>loading...</div>;
-  if (error || !data) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>
+  if (error || !data) return <div>failed to load</div>
   return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>
-          <Link href={item.url} target="_blank">
-            {item.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
-export default PostList;
+    <>
+      <ul>
+        {data.map((items, index) => {
+          return items.map((item) => (
+            <li key={item.id}>
+              <Link href={item.url} target="_blank">
+                {item.title} ({item.time_ago})
+              </Link>
+            </li>
+          ))
+        })}
+      </ul>
+      <button onClick={() => setSize(size + 1)}>Load More</button>
+    </>
+  )
+}
+export default PostList
